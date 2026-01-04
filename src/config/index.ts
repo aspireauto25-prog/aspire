1b724a0f82156bd7c759dfdcba1e202c8037f781
Samarajya Shrestha <samarajyastha@gmail.com>
2026-01-04 18:55:36 +0545

    feat: create db connection

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,10 @@
 const config = {
   appName: process.env.NEXT_PUBLIC_APP_NAME || "",
+  cloudinaryApiKey: "",
+  cloudinaryApiSecret: "",
+  cloudinaryCloudName: "",
+  supabaseKey: "",
+  supabaseUrl: "",
 };
 
 export default config;
