765be60db71972a72683e441fd37970baa347103
Samarajya Shrestha <samarajyastha@gmail.com>
2026-01-09 22:52:04 +0545

    feat: create car api

---
+++ src/lib/schemas/car.schema.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+export const carSchema = z.object({
+  brand: z.string("Car brand is required.").trim().min(2),
+  category: z.string().trim().optional(),
+  chassis_number: z.string("Chassis number is required.").trim().min(2),
+  color: z.string().trim().optional(),
+  description: z.string().min(10, "Car description is too short.").optional(),
+  drive_type: z.string().trim().optional(),
+  engine_capacity: z.number().optional(),
+  engine_number: z.string().trim().optional(),
+  features: z.string().optional(),
+  fuel_type: z.string().trim().optional(),
+  license_plate: z.string("License plate is required.").trim().min(2),
+  mileage: z.number().optional(),
+  model: z.string("Car model is required.").trim().min(2),
+  price: z.number("Car price is required."),
+  seat_capacity: z.number().optional(),
+  transmission_type: z.string().trim().optional(),
+  variant: z.string().trim().optional(),
+  year: z.number({
+    error: (value) =>
+      value.input === undefined ? "Year is required." : "Invalid year value.",
+  }),
+});
