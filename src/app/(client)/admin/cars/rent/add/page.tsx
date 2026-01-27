3b7c21ec66003f8a587c7597aca21d0174f571b0
samarajya <samarajya@dzangolab.com>
2026-01-27 18:58:08 +0545

    chore: fix lint

--- src/app/(client)/admin/cars/rent/add/page.tsx
+++ src/app/(client)/admin/cars/rent/add/page.tsx
@@ -1,7 +1,5 @@
 import RentCarForm from "@/components/admin/cars/rent/Form";
 
-export const dynamic = "force-dynamic";
-
 const AddCarRentPage = () => <RentCarForm />;
 
 export default AddCarRentPage;
--- src/app/(client)/admin/cars/sell/add/page.tsx
+++ src/app/(client)/admin/cars/sell/add/page.tsx
@@ -1,7 +1,5 @@
 import SaleCarForm from "@/components/admin/cars/sale/Form";
 
-export const dynamic = "force-dynamic";
-
 const AddCarSalePage = () => <SaleCarForm />;
 
 export default AddCarSalePage;
