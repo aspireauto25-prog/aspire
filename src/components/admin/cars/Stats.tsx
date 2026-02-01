4780763937fafa11944a75b9d91d567289b25b90
samarajya <samarajya@dzangolab.com>
2026-02-01 19:03:10 +0545

    chore: update stats

--- src/components/admin/cars/Stats.tsx
+++ src/components/admin/cars/Stats.tsx
@@ -1,17 +1,11 @@
+import { FaCar, FaCheck, FaKey, FaTools } from "react-icons/fa";
+
 import { getCountByStatus } from "@/api/cars";
 import {
   CAR_STATUS_AVAILABLE,
   CAR_STATUS_MAINTENANCE,
   CAR_STATUS_RENTED,
 } from "@/constants/cars";
-import {
-  FaArrowDown,
-  FaArrowUp,
-  FaCar,
-  FaCheck,
-  FaKey,
-  FaTools,
-} from "react-icons/fa";
 
 const CarStats = async () => {
   const data = await getCountByStatus();
@@ -48,9 +42,6 @@ const CarStats = async () => {
             <FaCar className="text-blue-500 text-xl" />
           </div>
         </div>
-        <p className="text-green-600 text-sm mt-2 flex items-center">
-          <FaArrowUp className=" mr-1" /> 12% from last month
-        </p>
       </div>
       <div className="bg-white dark:bg-gray-800 rounded-xl shadow p-5 border-l-4 border-green-500">
         <div className="flex justify-between items-center">
@@ -64,9 +55,6 @@ const CarStats = async () => {
             <FaCheck className="text-green-500 text-xl" />
           </div>
         </div>
-        <p className="text-green-600 text-sm mt-2 flex items-center">
-          <FaArrowUp className="mr-1" /> 8% from last month
-        </p>
       </div>
       <div className="bg-white dark:bg-gray-800 rounded-xl shadow p-5 border-l-4 border-purple-500">
         <div className="flex justify-between items-center">
@@ -80,9 +68,6 @@ const CarStats = async () => {
             <FaKey className="text-purple-500 text-xl" />
           </div>
         </div>
-        <p className="text-red-600 text-sm mt-2 flex items-center">
-          <FaArrowDown className="mr-1" /> 3% from last month
-        </p>
       </div>
       <div className="bg-white dark:bg-gray-800 rounded-xl shadow p-5 border-l-4 border-amber-500">
         <div className="flex justify-between items-center">
@@ -96,9 +81,6 @@ const CarStats = async () => {
             <FaTools className="text-amber-500 text-xl" />
           </div>
         </div>
-        <p className="text-green-600 text-sm mt-2 flex items-center">
-          <FaArrowUp className=" mr-1" /> 5% from last month
-        </p>
       </div>
     </div>
   );
