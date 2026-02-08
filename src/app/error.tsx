fbdb0e49880f596dc919c7c6af25957b421c6e40
Samarajya Shrestha <samarajyastha@gmail.com>
2026-02-08 21:49:18 +0545

    feat: implement error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,10 +1,37 @@
 "use client";
 
-import ErrorComponent from "@/components/ErrorComponent";
-import { AppError } from "@/helpers/errorNormalization";
+import { FaHome } from "react-icons/fa";
 
-const ErrorHandler = ({ error }: { error: Error | AppError }) => {
-  return <ErrorComponent defaultError="Oops!" error={error} />;
+import { AppError } from "@/helpers/errorNormalization";
+import { HOME_ROUTE } from "@/constants/routes";
+import Button from "@/components/Button";
+import ErrorComponent from "@/components/ErrorComponent";
+import OutlinedButton from "@/components/OutlinedButton";
+
+interface ErrorHandlerProps {
+  error: Error | AppError;
+  reset: () => void;
+}
+
+const ErrorHandler = ({ error, reset }: ErrorHandlerProps) => {
+  return (
+    <section className="py-16">
+      <div className="max-w-4xl mx-auto px-6">
+        <ErrorComponent
+          defaultError="Oops, Something went wrong!!!"
+          error={error}
+        />
+        <div className="flex items-center gap-4 mt-6">
+          <OutlinedButton onClick={reset} size="md">
+            Try Again
+          </OutlinedButton>
+          <Button href={HOME_ROUTE} size="md">
+            <FaHome /> Go Back Home
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ErrorHandler;
