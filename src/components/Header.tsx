d46acb99b1caba5303608d6fb82b89b196c01977
samarajya <samarajya@dzangolab.com>
2026-02-09 19:41:38 +0545

    feat: implement mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { FaBars, FaCalendarAlt } from "react-icons/fa";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import Link from "next/link";
 
 import { ADMIN_ROUTE, CONTACT_ROUTE, HOME_ROUTE } from "@/constants/routes";
@@ -11,6 +12,8 @@ import navlinks from "@/constants/navlinks";
 import Theme from "./Theme";
 
 const Header = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
   const pathname = usePathname();
 
   const isHomeRoute = pathname === HOME_ROUTE;
@@ -66,11 +69,46 @@ const Header = () => {
               <FaCalendarAlt />
               <span>Enquire Now</span>
             </Button>
-            <button className="lg:hidden">
+            <button
+              className="lg:hidden cursor-pointer"
+              onClick={() => setShowMobileMenu(true)}
+            >
               <FaBars className="text-xl" />
             </button>
           </div>
         </div>
+        {/* Mobile menu  */}
+        {showMobileMenu && (
+          <div className="bg-white dark:bg-gray-950 px-4 pt-2 pb-4 rounded-2xl mt-2" onClick={() => setShowMobileMenu(false)}>
+            <nav className="flex flex-col lg:hidden">
+              {navlinks.map((navlink, index) => {
+                const isActive = pathname === navlink.route;
+
+                return (
+                  <Link
+                    key={index}
+                    href={navlink.route}
+                    className={`font-medium hover:text-primary transition-colors relative group py-2 ${
+                      isActive ? "text-primary" : ""
+                    }`}
+                  >
+                    {navlink.label}
+                    <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-primary group-hover:w-full transition-all duration-300" />
+                  </Link>
+                );
+              })}
+            </nav>
+            <Button
+              href={`${CONTACT_ROUTE}#contact-form`}
+              size="md"
+              rounded
+              className="flex sm:hidden mt-4"
+            >
+              <FaCalendarAlt />
+              <span>Enquire Now</span>
+            </Button>
+          </div>
+        )}
       </div>
     </header>
   );
