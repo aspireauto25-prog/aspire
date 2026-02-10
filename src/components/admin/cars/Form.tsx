d355466966d917a3bf589cf6abc9cd9f5f8f8465
samarajya <samarajya@dzangolab.com>
2026-02-10 10:06:36 +0545

    chore: fix lint

--- src/components/admin/cars/Form.tsx
+++ src/components/admin/cars/Form.tsx
@@ -249,7 +249,7 @@ const CarForm = ({ car, mode = "create" }: Props) => {
                 htmlFor="variant"
                 className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1"
               >
-                Variant / Trim
+                Variant
               </label>
               <input
                 type="text"
