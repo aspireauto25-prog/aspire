d75d63e9416a74abb2f3e2858405d8ce5e130df2
samarajya <samarajya@dzangolab.com>
2026-02-23 18:33:14 +0545

    fix: update address

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -18,7 +18,7 @@ export const WHATSAPP_URL = "https://wa.me/0497790788";
 
 export const PHONE = "0497790788";
 export const EMAIL = "info@aspireauto.com.au";
-export const LOCATION = "29 Foxwood Way, Langford WA 6147";
+export const LOCATION = "29B Foxwood Way, Langford WA 6147";
 
 export const contactFilters = [
   { label: "All", value: "" },
